4f10ec74f375ef32aed69080a6f2090dbf4c343d
tkeiot <tkeiotroom@gmail.com>
2024-10-05 07:53:25 +0700

    Add files via upload

---
+++ globals.py
@@ -0,0 +1,46 @@
+volume=50
+volume_mic = 35
+led_mute = 0
+text=''
+player=''
+TextYoube=''
+value = None
+radio_channel = 0
+radio_play=0
+client_id=0
+value_disp = None
+next_play=0
+#=============
+prompt=''
+count = 0
+check=0
+essid = None
+ip_wifi= None
+sig_wifi = 0
+enb_lcd = 0
+connect_assistant = 0
+api_picovoice = None
+api_gemini = None
+temperature = None
+humidity = None
+
+delete_file = 0
+url = None
+ip_wifi = None
+ip_home_assistant = 0
+
+text_send = None
+ok_chatbot = 0
+playing = False
+playing_ok = False
+process = None
+text_zing = None
+text_youtube = None
+stop_zing = 0
+stop_bt_zing = 0
+chech_assistant = None
+check_play = 0
+enb_english = None
+enb_vietnamese = None
+
+
